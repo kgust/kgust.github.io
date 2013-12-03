3d38d7a010d0e588dba69537dac3f585241862d7
Kevin Gustavson <kdgustavson@gmail.com>
2013-12-02 18:13:56 -0600

    More updates.

--- js/contact.js
+++ js/contact.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
 	$('#contact-form input.contact, #contact-form a.contact').click(function (e) {
 		e.preventDefault();
 		// load the contact form using ajax
-		$.get("data/contact.php", function(data){
+		$.get("http://kevingustavson-dtedesigns.rhcloud.com/data/contact.php", function(data) {
 			// create a modal dialog with the data
 			$(data).modal({
 				closeHTML: "<a href='#' title='Close' class='modal-close'>x</a>",
@@ -114,7 +114,7 @@ var contact = {
 				}, function () {
 					$('#contact-container .contact-loading').fadeIn(200, function () {
 						$.ajax({
-							url: 'data/contact.php',
+							url: 'http://kevingustavson-dtedesigns.rhcloud.com/data/contact.php',
 							data: $('#contact-container form').serialize() + '&action=send',
 							type: 'post',
 							cache: false,
